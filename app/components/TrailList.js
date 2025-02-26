603b9d4028a19ca117bbf214dacb88f933ee8a56
stuja16 <114200014+stuja16@users.noreply.github.com>
2025-02-26 15:17:00 -0600

    Add hiking and biking icons to trail list

--- app/components/TrailList.js
+++ app/components/TrailList.js
@@ -1,46 +1,92 @@
-import React from 'react';
-import {FlatList, StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+
+import colors from "../config/colors";
 
 const TrailList = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
+    <View style={styles.background}>
       <FlatList
         data={[
-          {key: 'Newberry Trail'},
-          {key: 'Hydroelectric Heritage Trailway'},
-          {key: 'Telulah Park'},
-          {key: 'Ellen Kort Peace Park'},
+          { key: "Newberry Trail", length: 1.75, hiking: true },
+          { key: "Hydroelectric Heritage Trailway" },
+          { key: "Telulah Park" },
+          { key: "Ellen Kort Peace Park" },
+          { key: "Old Stone Bridge Trail" },
+          { key: "Peabody Park" },
+          { key: "Trestle Trail" },
+          { key: "Loop the Little Lake Trail" },
+          { key: "Bubolz Nature Preserve" },
+          { key: "Heckrodt Wetland Reserve" },
+          { key: "Example Trail" },
         ]}
-        renderItem={({item}) => (  
-            <TouchableWithoutFeedback onPress={ () => {navigation.navigate('Info', {
-              trailName: item.key,
-            })}}>
-              <Text style={styles.item}>{item.key}</Text>
-            </TouchableWithoutFeedback>
+        renderItem={({ item }) => (
+          <TouchableWithoutFeedback
+            onPress={() => {
+              navigation.navigate("Info", {
+                trailName: item.key,
+              });
+            }}
+          >
+            <View style={styles.item}>
+              <View>
+                <Text style={styles.name}>{item.key}</Text>
+                <Text style={styles.length}>{item.length || "?"} miles</Text>
+              </View>
+              <View style={styles.iconRow}>
+                <FontAwesome6
+                  name="person-hiking"
+                  size={24}
+                  color={colors.primary}
+                />
+                <FontAwesome6
+                  name="person-biking"
+                  size={24}
+                  color={colors.primary}
+                />
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
         )}
       />
     </View>
   );
 };
 
-/*actionOnRow(item) {
-  const navigation = useNavigation();
-  console.log('Selected Item :',item.key);
-  navigation.navigate('Info');
-};*/
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  background: {
     paddingTop: 22,
+    backgroundColor: colors.background,
   },
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    height: 70,
+  },
+  name: {
+    fontSize: 25,
+  },
+  length: {
+    fontSize: 17,
+    color: "grey",
+  },
+  iconRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    gap: 20,
   },
 });
 
